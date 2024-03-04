13fbcaf14bde55883bbcd5aa8a299d62549b3f75
zorojir999 <zorojir999@gmail.com>
2024-03-04 17:57:30 +0600

    zoro

--- movie_app/models.py
+++ movie_app/models.py
@@ -30,3 +30,19 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review for {self.movie.title} by {self.user.username}"
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -1,18 +1,26 @@
-
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import Director, Movie, Review
+
+def validate_text_min_length(value, min_length):
+    if len(value) < min_length:
+        raise ValidationError(f'Минимальная длина для этого поля должна быть {min_length}')
+    return value
 
 class DirectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
-        fields = ['id', 'name']
+        fields = '__all__'
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'description', 'duration', 'director']
+        fields = '__all__'
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ['id', 'text', 'movie']
+        fields = '__all__'
+
+    def validate_text(self, value):
+        return validate_text_min_length(value, 5)
