09cf6aa939924ccc4e890f4e1befce6d481e12ae
zorojir999 <zorojir999@gmail.com>
2024-03-09 15:55:22 +0600

    zoro

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import movie_list, movie_detail, director_list, movie_review_list
+from .views import MovieList, MovieDetail, DirectorList, MovieReviewList
 
 urlpatterns = [
-    path('movies/', movie_list, name='movie-list'),
-    path('movies/<int:pk>/', movie_detail, name='movie-detail'),
-    path('directors/', director_list, name='director-list'),
-    path('movies/reviews/', movie_review_list, name='movie-review-list'),
-
+    path('movies/', MovieList.as_view(), name='movie-list'),
+    path('movies/<int:pk>/', MovieDetail.as_view(), name='movie-detail'),
+    path('directors/', DirectorList.as_view(), name='director-list'),
+    path('movies/reviews/', MovieReviewList.as_view(), name='movie-review-list'),
 ]
+
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,105 +1,48 @@
-from django.db.models import Avg, Count
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.db.models import Count, Avg
 from .models import Movie, Director, Review
 from .serializers import MovieSerializer, DirectorSerializer, ReviewSerializer
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 
+class MovieList(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 
-@api_view(['GET'])
-def movie_list(request):
-    if request.method == 'GET':
-        queryset = Movie.objects.all()
-        serializer = MovieSerializer(queryset, many=True)
-        return Response(serializer.data, status=200)
-    elif request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         serializer = MovieSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def movie_detail(request, pk):
-    try:
-        movie = Movie.objects.get(pk=pk)
-    except Movie.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class DirectorList(generics.ListCreateAPIView):
+    queryset = Director.objects.annotate(movies_count=Count('movie'))
+    serializer_class = DirectorSerializer
 
-    if request.method == 'GET':
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = MovieSerializer(movie, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=200)
-    elif request.method == 'DELETE':
-        movie.delete()
-        return Response(status=204)
-
-
-
-@api_view(['GET', 'POST'])
-def director_list(request):
-    if request.method == 'GET':
-        queryset = Director.objects.annotate(movies_count=Count('movie'))
-        for director in queryset:
-            director.movies_count = Movie.objects.filter(director_id=director.pk).count()
-        serializer = DirectorSerializer(queryset, many=True)
-        return Response(serializer.data, status=200)
-    elif request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         serializer = DirectorSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+class DirectorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def director_detail(request, id):
-    director = get_object_or_404(Director, id=id)
-    if request.method == 'GET':
-        serializer = DirectorSerializer(director)
-        return Response(serializer.data, status=200)
-    elif request.method == 'PUT':
-        serializer = DirectorSerializer(director, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=201)
-    elif request.method == 'DELETE':
-        director.delete()
-        return Response(status=204)
+class MovieReviewList(generics.ListCreateAPIView):
+    queryset = Movie.objects.prefetch_related('review_set')
+    serializer_class = MovieSerializer
 
-
-@api_view(['GET', 'POST'])
-def movie_review_list(request):
-    if request.method == 'GET':
-        queryset = Movie.objects.prefetch_related('review_set')
-        for movie in queryset:
-            movie.avg_rating = movie.review_set.aggregate(Avg('stars'))['stars__avg']
-        serializer = MovieSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         serializer = MovieSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def movie_review_detail(request, id):
-    review = get_object_or_404(Review, id=id)
-
-    if request.method == 'GET':
-        serializer = ReviewSerializer(review)
-        return Response(serializer.data, status=200)
-    elif request.method == 'PUT':
-        serializer = ReviewSerializer(review, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=200)
-    elif request.method == 'DELETE':
-        review.delete()
-        return Response(status=204)
-
+class MovieReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
